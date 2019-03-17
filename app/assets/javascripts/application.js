077146fac68da9e6286af211053f083d4e46f8a8
drewmyers <drewmyerssss@gmail.com>
2019-03-17 15:15:43 +0000

    commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require popper
 //= require tether
 //= require bootstrap-sprockets
+//= require bootstrap
 //= require_tree .
